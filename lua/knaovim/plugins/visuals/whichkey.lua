2f66d69c293a4a18f2d4564822496ad4b3b36e19
John Osinski <johnosinski80@gmail.com>
2026-03-14 10:46:29 -0400

    update menu definitions in whichkey

--- lua/knaovim/plugins/visuals/whichkey.lua
+++ lua/knaovim/plugins/visuals/whichkey.lua
@@ -6,6 +6,7 @@ return {
             require("which-key").add({
                 { '<leader>g', group = '[g]it actions' },
                 { '<leader>l', group = '[l]sp actions' },
+                { '<leader>i', group = '[i]nterface actions' },
                 -- { '<leader>t', group = '[t]ray toggles' },
                 -- { '<leader>f', group = '[f]ind something' },
                 -- { '<leader>n', group = '[n]nn/oil' },
