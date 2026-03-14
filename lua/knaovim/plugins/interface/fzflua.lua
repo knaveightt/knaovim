33586c4420f26ced0a881f06e6be366b6364bbb7
John Osinski <johnosinski80@gmail.com>
2026-03-14 12:25:58 -0400

    restructured FzfLua keybinds

--- lua/knaovim/plugins/interface/fzflua.lua
+++ lua/knaovim/plugins/interface/fzflua.lua
@@ -6,12 +6,12 @@ return {
     keys = {
         {"<leader>ff", "<cmd>FzfLua files<CR>", desc="find [f]iles"}, 
         {"<leader>fb", "<cmd>FzfLua buffers<CR>", desc="find [b]uffers"}, 
-        {"<leader>fS", "<cmd>FzfLua grep<CR>", desc="grep [S]earch"}, 
-        {"<leader>fs", "<cmd>FzfLua live_grep<CR>", desc="live [g]rep"}, 
-        {"<leader>fgc", "<cmd>FzfLua git_commits<CR>", desc="find git [c]ommits"}, 
-        {"<leader>fgC", "<cmd>FzfLua git_bcommits<CR>", desc="buffer [C]ommits"}, 
-        {"<leader>fgs", "<cmd>FzfLua git_status<CR>", desc="git [s]tatus search"}, 
+        {"<leader>fG", "<cmd>FzfLua grep<CR>", desc="[G]rep search"}, 
+        {"<leader>fg", "<cmd>FzfLua live_grep<CR>", desc="live [g]rep"}, 
         {"<leader>fh", "<cmd>FzfLua helptags<CR>", desc="find [h]elp"}, 
         {"<leader>f?", "<cmd>FzfLua builtin<CR>", desc="[?]see all"}, 
+        {"<leader>gfl", "<cmd>FzfLua git_commits<CR>", desc="git commit [l]og"}, 
+        {"<leader>gfc", "<cmd>FzfLua git_bcommits<CR>", desc="buffer [C]ommits"}, 
+        {"<leader>gfs", "<cmd>FzfLua git_status<CR>", desc="git [s]tatus search"}, 
     },
 }
--- lua/knaovim/plugins/visuals/whichkey.lua
+++ lua/knaovim/plugins/visuals/whichkey.lua
@@ -5,11 +5,11 @@ return {
         require("which-key").setup({
             require("which-key").add({
                 { '<leader>g', group = '[g]it actions' },
+                { '<leader>gf', group = '[f]ind something' },
                 { '<leader>l', group = '[l]sp actions' },
                 { '<leader>i', group = '[i]nterface actions' },
                 -- { '<leader>t', group = '[t]ray toggles' },
                 { '<leader>f', group = '[f]ind something' },
-                { '<leader>fg', group = 'find in [g]it' },
                 -- { '<leader>n', group = '[n]nn/oil' },
                 { '<leader>w', group = '[w]indow actions' },
                 { '<leader>b', group = '[b]uffer actions' },
