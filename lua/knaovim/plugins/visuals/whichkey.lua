48e9b3c345ec14e4568e9abc3c1a77bc5931298c
John Osinski <johnosinski80@gmail.com>
2026-03-14 02:00:58 -0400

    added whichkey plugin

---
+++ lua/knaovim/plugins/visuals/whichkey.lua
@@ -0,0 +1,17 @@
+return {
+    "folke/which-key.nvim",
+    event = "VeryLazy",
+    config = function()
+        require("which-key").setup({
+            require("which-key").add({
+                -- { '<leader>g', group = '[g]it' },
+                { '<leader>l', group = '[l]sp actions' },
+                -- { '<leader>t', group = '[t]ray toggles' },
+                -- { '<leader>f', group = '[f]ind something' },
+                -- { '<leader>n', group = '[n]nn/oil' },
+                { '<leader>w', group = '[w]indow actions' },
+                { '<leader>b', group = '[b]uffer actions' },
+            })
+        })
+    end
+}
