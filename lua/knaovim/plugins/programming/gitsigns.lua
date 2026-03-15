97f378be31266367813fb290208d112091adb307
John Osinski <johnosinski80@gmail.com>
2026-03-14 20:01:11 -0400

    added keybind for git diff view

--- lua/knaovim/plugins/programming/gitsigns.lua
+++ lua/knaovim/plugins/programming/gitsigns.lua
@@ -2,4 +2,7 @@ return {
     "lewis6991/gitsigns.nvim",
     lazy = false,
     opts = {},
+    keys = {
+        { "<leader>gd", "<cmd>Gitsigns diffthis<CR>", desc = "line [d]iffview" },
+    },
 }
