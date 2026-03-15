1da233dc46b1b2ffe684c01a6570cb3dd4c2ddbb
John Osinski <johnosinski80@gmail.com>
2026-03-15 18:14:42 -0400

    added some convenience keybinds

--- lua/knaovim/core/keybinds.lua
+++ lua/knaovim/core/keybinds.lua
@@ -16,11 +16,38 @@ keymap.set("n", "<C-u>", "<C-u>zz")
 keymap.set("n", "n", "nzzzv")
 keymap.set("n", "N", "Nzzzv")
 keymap.set('n', '<ESC>', ':nohlsearch<CR>')
+keymap.set('n', "M", "M0")
 
 keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 keymap.set("v", "<", "<gv")
 keymap.set("v", ">", ">gv")
+keymap.set("x", "V", "j0") -- keep using Shift+v to select more lines
+
+local function is_at_bottom()
+    local win = 0
+    local cursor_screen_row = vim.fn.screenrow()
+    local window_height = vim.fn.winheight(win)
+    if cursor_screen_row-1 == window_height then
+        return true
+    else
+        return false
+    end
+end
+
+keymap.set("n", "H", function()
+    if vim.fn.screenrow() == 2 then
+        vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('<C-u>', true, false, true), 'n', false)
+    end
+    vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('H0', true, false, true), 'n', false)
+end, { desc = "smart-shift cursor and view upwards"})
+
+keymap.set("n", "L", function()
+    if is_at_bottom() then
+        vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('<C-d>', true, false, true), 'n', false)
+    end
+    vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('L0', true, false, true), 'n', false)
+end, { desc = "smart-shift cursor and view downwards"})
 
 keymap.set('n', '<leader>w', "<C-w>")
 keymap.set('n', '<leader>bd', ':bd<CR>', {desc="[d]elete buffer"})
