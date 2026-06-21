df98f38c1f78a41c93565ff0c59ff4be49eb03b6
John Osinski <johnosinski80@gmail.com>
2026-06-21 14:21:23 -0400

    added gitsigns plugin

---
+++ lua/knaovim/plugins/programming/gitsigns.lua
@@ -0,0 +1,8 @@
+return {
+    "lewis6991/gitsigns.nvim",
+    lazy = false,
+    opts = {},
+    keys = {
+        { "<leader>gd", "<cmd>Gitsigns diffthis<CR>", desc = "line [d]iffview" },
+    },
+}
